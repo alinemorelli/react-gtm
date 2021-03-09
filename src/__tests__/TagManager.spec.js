657927b2a79d3e81d90444616a66552f829ebb8d
Laurent Maillet <lmaillet@talend.com>
2021-03-09 12:01:07 +0100

    test: nonce

--- src/__tests__/TagManager.spec.js
+++ src/__tests__/TagManager.spec.js
@@ -16,4 +16,16 @@ describe('TagManager', () => {
     TagManager.initialize(gtmArgs)
     expect(window.dataLayer).toHaveLength(1)
   })
+
+  it('should render tagmanager with nonce', () => {
+    TagManager.initialize({gtmId: 'GTM-000000', nonce: 'foo'})
+    const scripts = window.document.getElementsByTagName('script');
+    expect(scripts[0].nonce).toBe('foo');
+  })
+
+  it('should render tagmanager without nonce', () => {
+    TagManager.initialize({gtmId: 'GTM-000000'})
+    const scripts = window.document.getElementsByTagName('script');
+    expect(scripts[0].nonce).toBe('');
+ })
 })
